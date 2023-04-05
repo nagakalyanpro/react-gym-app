4de3f84b1f28be887cbdfc30f4344c8c44cad12b
nagakalyanpro <77896080+nagakalyanpro@users.noreply.github.com>
2023-04-05 17:14:48 +0530

    gym-app

---
+++ src/projectfolder/pages/MainPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import TopBacC from '../components/TopBacC'
+import HeroC from '../components/HeroC'
+import AboutC from '../components/AboutC'
+import GalleryC from '../components/GalleryC'
+import SectionC from '../components/SectionC'
+import FooterC from '../components/FooterC'
+import PricingC from '../components/PricingC'
+
+const MainPage = () => {
+  return (
+    <div>
+        <TopBacC />
+        <HeroC />
+        <AboutC />
+        <SectionC />
+        <GalleryC />
+        <PricingC />
+        <FooterC />
+    </div>
+  )
+}
+
+export default MainPage
